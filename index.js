b4472d0b60e61d9a34ae5f9dc3420d9fd6756127
arjunsingh01 <arjun.singh01@nagarro.com>
2023-07-13 14:06:29 +0530

    use colors packages

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 #!node
 const packJson = require("./package.json");
 const welcome = require("cli-welcome");
+const { bold } = require("colors");
+const colors = require("colors/safe");
+const twitterColor = colors.bgBrightBlue;
+const githubColor = colors.bgBrightGreen;
+const linkedlinColor = colors.bgBrightCyan;
 welcome({
   title: `Get to know Arjun`,
+  tagLine: `Howdy , nice to meet ya!`,
   version: packJson.version,
   bgColor: `#fadc00`,
   color: `#000000`,
   bold: true,
-  clear: true,
+  clear: false,
 });
+
+console.log(colors.random("Arjun Kumar Singh"));
 console.log(`
-Welcome to my blog! In this post, I am thrilled to introduce myself,
-Arjun Kumar Singh, a dedicated and enthusiastic .NET developer. With 
-a passion for coding and a strong background in software development,
-I am excited to share my journey and experiences in the world of 
-technology. Join me as we explore the fascinating world of .NET and beyond!
+ 
+${colors.italic(`Welcome to my blog! In this post, I am thrilled to introduce myself,Arjun Kumar Singh, a dedicated and enthusiastic .NET developer. With 
+a passion for coding and a strong background in software development,I am excited to share my journey and experiences in the world of 
+technology. Join me as we explore the fascinating world of .NET and beyond!`)}
 
-Beyond the world of programming, I am deeply passionate about continuously 
-learning and staying up-to-date with the latest advancements in technology.
-I believe in the importance of self-improvement and regularly explore new 
-frameworks, tools, and best practices to enhance my skill set and deliver 
-top-notch solutions.
+${colors.dim(
+  "Beyond the world of programming, I am deeply passionate about continuously learning and staying up-to-date with the latest advancements in technology."
+)}
+ 
 
-🐤  Twitter:  https://twitter.com/arjun4512
-🐈‍⬛  GitHub :  https://github.com/arjun4512
-🔗  Linkedlin:https://linkedli.com/arjun.kumar.singh 
+🐤 ${twitterColor("Twitter:")}  ${colors.underline(
+  `https://twitter.com/arjun4512`
+)}
+
+🐈 ‍${githubColor("GitHub:")}  ${colors.underline(
+  "https://github.com/arjun4512"
+)}
+
+🔗 ${linkedlinColor("Linkedlin:")} ${colors.underline(
+  "https://linkedli.com/arjun.kumar.singh"
+)} 
 `);
